f0adc740346477d4637bd0c8327a8d352e2fc1bc
Dosen <ahmfarisi@gmail.com>
2023-11-09 13:13:37 +0700

    Update detail_screen.dart

--- wisata_bandung/lib/detail_screen.dart
+++ wisata_bandung/lib/detail_screen.dart
@@ -7,6 +7,7 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
+            child: SingleChildScrollView(
       child: Column(
         children: [
           Image.asset('images/farm-house.jpg'),
@@ -58,8 +59,31 @@ class DetailScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
+          SizedBox(
+            height: 150,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Image.network(
+                      'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Image.network(
+                      'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Image.network(
+                      'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                )
+              ],
+            ),
+          )
         ],
       ),
-    ));
+    )));
   }
 }
