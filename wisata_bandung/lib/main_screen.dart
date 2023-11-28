8c5b481af4008bd92dd094b60df0fecf849afa3a
umdp <ahmfarisi@gmail.com>
2023-11-28 11:18:15 +0700

    Repair pubspec on font and completing main dart with inkwell

--- wisata_bandung/lib/main_screen.dart
+++ wisata_bandung/lib/main_screen.dart
@@ -6,9 +6,39 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wisata Bandung'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Wisata Bandung'),
+        ),
+        body: InkWell(
+          onTap: () {},
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('images/farm-house.jpg'),
+                ),
+                const Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Farm House Lembang',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 10),
+                          Text('Lembang')
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
